f9d53a31a7cd67409756f946cfe15fb1a4154ace
Yavor <ygrancharov@ebay.com>
2023-08-30 11:53:37 +0300

    css namespace

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
+import Header from "./components/Header";
 import Marketing from "./components/Marketing";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Marketing />
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Marketing />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
